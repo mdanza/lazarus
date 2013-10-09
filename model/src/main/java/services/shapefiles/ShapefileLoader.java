84a7f627ef5ecdf1e1e30e73b9afeda473ef911a
scarrera <santiagouc@gmail.com>
2013-10-09 12:46:24 -0200

    read from shapefile tests

--- model/src/main/java/services/shapefiles/ShapefileLoader.java
+++ model/src/main/java/services/shapefiles/ShapefileLoader.java
@@ -2,73 +2,115 @@ package services.shapefiles;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.nio.channels.FileChannel;
 import java.nio.charset.Charset;
+import java.util.Collection;
+import java.util.Iterator;
 
-import org.geotools.data.FileDataStore;
-import org.geotools.data.FileDataStoreFinder;
+import org.geotools.data.FeatureReader;
+import org.geotools.data.FeatureSource;
+import org.geotools.data.shapefile.ShapefileDataStore;
 import org.geotools.data.shapefile.dbf.DbaseFileReader;
-import org.geotools.data.simple.SimpleFeatureSource;
-import org.geotools.map.FeatureLayer;
-import org.geotools.map.Layer;
-import org.geotools.map.MapContent;
-import org.geotools.styling.SLD;
-import org.geotools.styling.Style;
-import org.geotools.swing.JMapFrame;
-import org.geotools.swing.data.JFileDataStoreChooser;
+import org.geotools.feature.FeatureCollection;
+import org.opengis.feature.Feature;
+import org.opengis.feature.Property;
+import org.opengis.feature.type.FeatureType;
+import org.opengis.feature.type.PropertyDescriptor;
+import org.opengis.feature.type.PropertyType;
+import org.opengis.geometry.Geometry;
 
-/**
- * Prompts the user for a shapefile and displays the contents on the screen in a map frame.
- * <p>
- * This is the GeoTools Quickstart application used in documentationa and tutorials. *
- */
 public class ShapefileLoader {
 
-    /**
-     * GeoTools Quickstart demo application. Prompts the user for a shapefile and displays its
-     * contents on the screen in a map frame
-     */
-    public static void main(String[] args) throws Exception {
-        // display a data store file chooser dialog for shapefiles
-        File file = JFileDataStoreChooser.showOpenFile("dbf", null);
-        if (file == null) {
-            return;
-        }
-
-        
-        
-        FileInputStream fileInputStream = new FileInputStream(file.getAbsolutePath());
+	public static void readDbf(String url) throws Exception {
+		FileInputStream fileInputStream = new FileInputStream(url);
 		FileChannel in = fileInputStream.getChannel();
-        DbaseFileReader r = new DbaseFileReader( in, true, Charset.defaultCharset() );
-        Object[] fields = new Object[r.getHeader().getNumFields()]; 
-        int j=0;
-        while (r.hasNext() && j<20) {
-        	Object[] o = r.readEntry(fields); 
-        	for(int i = 0;i<o.length;i++){
-        		System.out.println(o[i]);
-        	}
-        	j++;
-        }
-        r.close();
-        in.close();
-        fileInputStream.close();
-        
-        
-        
-        
-        FileDataStore store = FileDataStoreFinder.getDataStore(file);
-        SimpleFeatureSource featureSource = store.getFeatureSource();
+		DbaseFileReader r = new DbaseFileReader(in, true,
+				Charset.defaultCharset());
+		Object[] fields = new Object[r.getHeader().getNumFields()];
+		int j = 0;
+		while (r.hasNext() && j < 20) {
+			Object[] o = r.readEntry(fields);
+			for (int i = 0; i < o.length; i++) {
+				System.out.println(o[i]);
+			}
+			j++;
+		}
+		r.close();
+		in.close();
+		fileInputStream.close();
+	}
 
-        // Create a map content and add our shapefile to it
-        MapContent map = new MapContent();
-        map.setTitle("Quickstart");
-        
-        Style style = SLD.createSimpleStyle(featureSource.getSchema());
-        Layer layer = new FeatureLayer(featureSource, style);
-        map.addLayer(layer);
+	public static void readShp(String url) throws IOException,
+			URISyntaxException {
+		URL shapeURL = new File(url).toURI().toURL();
 
-        // Now display the map
-        JMapFrame.showMap(map);
-    }
+		// get feature results
+		ShapefileDataStore store = new ShapefileDataStore(shapeURL);
+		String name = store.getTypeNames()[0];
+		FeatureSource source = store.getFeatureSource(name);
+		FeatureCollection fsShape = source.getFeatures();
 
+		// print out a feature type header and wait for user input
+		FeatureType ft = fsShape.getSchema();
+		System.out.println("FID\t");
+
+		Collection<PropertyDescriptor> properties = ft.getDescriptors();
+		System.out.println();
+		int propCount = properties.size();
+		System.out.println();
+		Iterator<PropertyDescriptor> propertiesItr = properties.iterator();
+		while (propertiesItr.hasNext()) {
+			PropertyDescriptor property = propertiesItr.next();
+
+			PropertyType at = property.getType();
+
+			if (!Geometry.class.isAssignableFrom(at.getClass())) {
+				System.out.print(at.getName() + "\t");
+			}
+		}
+
+		// now print out the feature contents (every non geometric attribute)
+		FeatureReader reader = store.getFeatureReader();
+		int count = 0;
+		while (reader.hasNext()) {
+			Feature feature = reader.next();
+			System.out.print(feature.getIdentifier().getID() + "\t");
+			Collection<? extends Property> values = feature.getValue();
+			Iterator<? extends Property> valuesItr = values.iterator();
+			while (valuesItr.hasNext()) {
+				Property value = valuesItr.next();
+
+				if (!(value instanceof Geometry)) {
+					System.out.print(value + "\t");
+				}
+			}
+
+			System.out.println();
+			count++;
+		}
+
+		reader.close();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+
+		// and finally print out every geometry in wkt format
+		reader = store.getFeatureReader();
+
+		while (reader.hasNext()) {
+			Feature feature = reader.next();
+			System.out.print(feature.getIdentifier().getID() + "\t");
+			System.out.println(feature.getDefaultGeometryProperty());
+			System.out.println();
+		}
+		
+		reader.close();
+		System.out.println();
+		System.out.println("ROW COUNT:" + count);
+		System.out.println("PROPERTIES COUNT:" + propCount);
+		System.out.println();
+	}
 }

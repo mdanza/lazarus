07c015abe586cf65ee55ee3412a9f8d4c3194361
scarrera <santiagouc@gmail.com>
2013-12-16 16:48:21 -0200

    added point deserializer

--- Lazarus/src/com/android/lazarus/model/serialization/PointSerializer.java
+++ Lazarus/src/com/android/lazarus/model/serialization/PointSerializer.java
@@ -6,15 +6,18 @@ import com.android.lazarus.model.Point;
 import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import com.google.gson.JsonParseException;
 
 public class PointSerializer implements JsonDeserializer<Point> {
 
 	@Override
-	public Point deserialize(JsonElement json, Type typeOfT,
+	public Point deserialize(JsonElement el, Type typeOfT,
 			JsonDeserializationContext context) throws JsonParseException {
-		// TODO Auto-generated method stub
+		JsonObject json = el.getAsJsonObject();
 		Point point = new Point();
+		point.setLatitude(json.get("x").getAsDouble());
+		point.setLongitude(json.get("y").getAsDouble());
 		return point;
 	}
 

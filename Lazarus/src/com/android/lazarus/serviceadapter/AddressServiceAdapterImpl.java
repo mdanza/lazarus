da0bb64e181a04f063f3b72e36a6c63fedcccfe4
scarrera <santiagouc@gmail.com>
2013-12-19 11:22:27 -0200

    added UTF-8 encoding to Address Adapter query params

--- Lazarus/src/com/android/lazarus/serviceadapter/AddressServiceAdapterImpl.java
+++ Lazarus/src/com/android/lazarus/serviceadapter/AddressServiceAdapterImpl.java
@@ -3,12 +3,12 @@ package com.android.lazarus.serviceadapter;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.lang.reflect.Type;
+import java.net.URLEncoder;
 import java.util.List;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
 
 import com.android.lazarus.helpers.ConstantsHelper;
 import com.android.lazarus.helpers.SerializationHelper;
@@ -24,11 +24,10 @@ public class AddressServiceAdapterImpl implements AddressServiceAdapter {
 	@Override
 	public List<String> getPossibleStreets(String token, String name) {
 		HttpClient client = HttpClientCreator.getNewHttpClient();
-		String uri = ConstantsHelper.REST_API_URL
+		String url = ConstantsHelper.REST_API_URL
 				+ "/addresses/possibleStreets?name=" + name;
-		uri = uri.replace(" ", "%20");
-		HttpGet request = new HttpGet(uri);
 		try {
+			HttpGet request = new HttpGet(URLEncoder.encode(url, "UTF-8"));
 			request.addHeader("Authorization", token);
 			HttpResponse response = client.execute(request);
 			BufferedReader rd = new BufferedReader(new InputStreamReader(
--- Lazarus/src/com/android/lazarus/serviceadapter/utils/HttpClientCreator.java
+++ Lazarus/src/com/android/lazarus/serviceadapter/utils/HttpClientCreator.java
@@ -18,26 +18,29 @@ import org.apache.http.protocol.HTTP;
 
 public class HttpClientCreator {
 	public static HttpClient getNewHttpClient() {
-	    try {
-	        KeyStore trustStore = KeyStore.getInstance(KeyStore.getDefaultType());
-	        trustStore.load(null, null);
+		try {
+			KeyStore trustStore = KeyStore.getInstance(KeyStore
+					.getDefaultType());
+			trustStore.load(null, null);
 
-	        SSLSocketFactory sf = new MySSLSocketFactory(trustStore);
-	        sf.setHostnameVerifier(SSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
+			SSLSocketFactory sf = new MySSLSocketFactory(trustStore);
+			sf.setHostnameVerifier(SSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
 
-	        HttpParams params = new BasicHttpParams();
-	        HttpProtocolParams.setVersion(params, HttpVersion.HTTP_1_1);
-	        HttpProtocolParams.setContentCharset(params, HTTP.UTF_8);
+			HttpParams params = new BasicHttpParams();
+			HttpProtocolParams.setVersion(params, HttpVersion.HTTP_1_1);
+			HttpProtocolParams.setContentCharset(params, HTTP.UTF_8);
 
-	        SchemeRegistry registry = new SchemeRegistry();
-	        registry.register(new Scheme("http", PlainSocketFactory.getSocketFactory(), 80));
-	        registry.register(new Scheme("https", sf, 443));
+			SchemeRegistry registry = new SchemeRegistry();
+			registry.register(new Scheme("http", PlainSocketFactory
+					.getSocketFactory(), 80));
+			registry.register(new Scheme("https", sf, 443));
 
-	        ClientConnectionManager ccm = new ThreadSafeClientConnManager(params, registry);
+			ClientConnectionManager ccm = new ThreadSafeClientConnManager(
+					params, registry);
 
-	        return new DefaultHttpClient(ccm, params);
-	    } catch (Exception e) {
-	        return new DefaultHttpClient();
-	    }
+			return new DefaultHttpClient(ccm, params);
+		} catch (Exception e) {
+			return new DefaultHttpClient();
+		}
 	}
 }

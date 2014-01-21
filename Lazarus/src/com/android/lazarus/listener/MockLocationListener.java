d2fe1ce2dd3ae082cafcf8a15da503f1560f1236
scarrera <santiagouc@gmail.com>
2014-01-21 01:38:48 -0200

    added test positions for bus ride and transshipments states tests

--- Lazarus/src/com/android/lazarus/listener/MockLocationListener.java
+++ Lazarus/src/com/android/lazarus/listener/MockLocationListener.java
@@ -16,15 +16,46 @@ public class MockLocationListener extends LocationListenerImpl {
 	private VoiceInterpreterActivity voiceInterpreterActivity;
 	String provider = null;
 	Boolean restarted = false;
-	private String[] points = { "-34.90111,-56.14013", "-34.901377,-56.14007",
-			"-34.90162,-56.13995", "-34.90162,-56.13995",
-			"-34.901736,-56.140301", "-34.901848,-56.140513",
-			"-34.90171,-56.14033", "-34.901978,-56.140757",
-			"-34.90171,-56.14033", "-34.902242,-56.141253",
-			"-34.90254,-56.14187", "-34.90324,-56.14286",
-			"-34.90432,-56.14469", "-34.90432,-56.14469",
-			"-34.90490,-56.14446", "-34.90614,-56.14412",
-			"-34.90614,-56.14412", "-34.90624,-56.14460" };
+	// private String[] points = { "-34.90111,-56.14013",
+	// "-34.901377,-56.14007",
+	// "-34.90162,-56.13995", "-34.90162,-56.13995",
+	// "-34.901736,-56.140301", "-34.901848,-56.140513",
+	// "-34.90171,-56.14033", "-34.901978,-56.140757",
+	// "-34.90171,-56.14033", "-34.902242,-56.141253",
+	// "-34.90254,-56.14187", "-34.90324,-56.14286",
+	// "-34.90432,-56.14469", "-34.90432,-56.14469",
+	// "-34.90490,-56.14446", "-34.90614,-56.14412",
+	// "-34.90614,-56.14412", "-34.90624,-56.14460" };
+
+	// alternative: para prueba bus D11
+	private String[] points = { "-34.90111,-56.14013", "-34.90126,-56.140074",
+			"-34.901366,-56.140042", "-34.90163,-56.139935",
+			"-34.901568,-56.139634", "-34.901489,-56.139334",
+			"-34.901384,-56.139001", "-34.901252,-56.138583",
+			"-34.901181,-56.138314", "-34.899518,-56.133401",
+			"-34.897442,-56.124678", "-34.891563,-56.112286",
+			"-34.891053,-56.095045", "-34.893526,-56.092535",
+			"-34.890718,-56.071409", "-34.888906,-56.066431",
+			"-34.888105,-56.06391", "-34.887295,-56.061711",
+			"-34.886292,-56.058814", "-34.88623,-56.058578" };
+	
+	// alternative: para prueba bus D11 conexión L21
+	// private String[] points = { "-34.90111,-56.14013",
+	// "-34.90126,-56.140074",
+	// "-34.901366,-56.140042", "-34.90163,-56.139935",
+	// "-34.901568,-56.139634", "-34.901489,-56.139334",
+	// "-34.901384,-56.139001", "-34.901252,-56.138583",
+	// "-34.901181,-56.138314", "-34.899518,-56.133401",
+	// "-34.897442,-56.124678", "-34.891563,-56.112286",
+	// "-34.891053,-56.095045", "-34.893526,-56.092535",
+	// "-34.890718,-56.071409", "-34.888906,-56.066431",
+	// "-34.888105,-56.06391", "-34.887295,-56.061711",
+	// "-34.886292,-56.058814", "-34.88623,-56.058578",
+	// "-34.885773,-56.05743","-34.885288,-56.055788","-34.884831,-56.054533"
+	// ,"-34.883942,-56.05464","-34.882921,-56.055198","-34.879858,-56.056722"
+	// ,"-34.879251,-56.057011","-34.879515,-56.056915",
+	// "-34.879832,-56.056775",
+	// "-34.880157,-56.056614"};
 
 	public MockLocationListener(
 			VoiceInterpreterActivity voiceInterpreterActivity) {
@@ -98,16 +129,16 @@ public class MockLocationListener extends LocationListenerImpl {
 		protected String doInBackground(String... args) {
 			List<Location> locations = getLocationsFromPoints(args);
 			for (Location location : locations) {
-				if(!restarted){
-				try {
-					synchronized (locations) {
-						locations.wait(3000);
-					}
-				} catch (InterruptedException e) {
+				if (!restarted) {
+					try {
+						synchronized (locations) {
+							locations.wait(3000);
+						}
+					} catch (InterruptedException e) {
 
-				}
-				if (locations.indexOf(location) != 0)
-					onLocationChanged(location);
+					}
+					if (locations.indexOf(location) != 0)
+						onLocationChanged(location);
 				}
 			}
 			return "Walking";
@@ -116,7 +147,8 @@ public class MockLocationListener extends LocationListenerImpl {
 
 	public void restart() {
 		restarted = true;
-		MockLocationListener mockLocationListener = new MockLocationListener(this.voiceInterpreterActivity);
-		voiceInterpreterActivity.setLocationListener(mockLocationListener);		
+		MockLocationListener mockLocationListener = new MockLocationListener(
+				this.voiceInterpreterActivity);
+		voiceInterpreterActivity.setLocationListener(mockLocationListener);
 	}
 }
